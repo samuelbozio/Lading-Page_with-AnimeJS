16e86cf36c4dc41f9128d7930f47a4a1aa117874
Samuel Bozio <samuel@rodosoft.com.br>
2024-03-04 16:23:25 -0300

    landing page com um pouquinho de animejs

---
+++ index.js
@@ -0,0 +1,10 @@
+anime({
+   targets: '.square',
+   backgroundColor: '#FFF',
+   borderRadius: ['0%', '50%'],
+   easing: 'easeInOutQuad',
+   loop: true,
+   direction: 'alternate', // Adicione esta linha
+   endDelay: 1500,
+   duration: 3000
+});
